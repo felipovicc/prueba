6eaf491948d908af47e989a9cb6e3efe8109ec54
FelipeNL <felipovicc@gmail.com>
2024-07-12 17:01:55 +0200

    Setup repo and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  prettierRecommended,
+  prettierConfig,
+];
